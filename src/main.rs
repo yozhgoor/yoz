60c6f1084bd1a9163b939072e6a82a65f2036ada
yozhgoor <yozhgoor@outlook.com>
2022-02-07 05:50:57 +0100

    Implement the `new` subcommand

--- src/main.rs
+++ src/main.rs
@@ -25,22 +25,33 @@ enum SubCommand {
         #[clap(long)]
         no_terminal: bool,
     },
+    /// Create a new Rust project with some defaults.
+    New {
+        /// Name of the Rust project.
+        name: String,
+        #[clap(long)]
+        /// Create a new Rust library.
+        lib: bool,
+        /// Create a new Rust xtask project.
+        #[clap(long)]
+        xtask: bool,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
     let opt: Opt = clap::Parser::parse();
+
+    let working_dir = if let Some(current_dir) = opt.path {
+        current_dir
+    } else {
+        env::current_dir().expect("cannot get current directory")
+    };
 
     match opt.cmd {
         SubCommand::Launch {
             command,
             no_terminal,
         } => {
-            let working_dir = if let Some(current_dir) = opt.path {
-                current_dir
-            } else {
-                env::current_dir().expect("cannot get current directory")
-            };
-
             let mut main_process = if command.is_empty() {
                 let mut main_process = process::Command::new("nvim");
                 main_process.current_dir(&working_dir);
@@ -84,6 +95,37 @@ fn main() -> anyhow::Result<()> {
             if let Some(mut child) = terminal_process {
                 child.kill()?;
                 child.wait()?;
+            }
+        }
+        SubCommand::New { name, lib, xtask } => {
+            if working_dir.join(&name).exists() {
+                panic!("destination already exists");
+            }
+
+            if !lib && !xtask {
+                anyhow::ensure!(
+                    std::process::Command::new("cargo")
+                        .current_dir(&working_dir)
+                        .args(["new", &name])
+                        .status()
+                        .expect("`cargo new` command failed")
+                        .success(),
+                    "cannot create new project"
+                );
+            } else if lib && !xtask {
+                anyhow::ensure!(
+                    std::process::Command::new("cargo")
+                        .current_dir(&working_dir)
+                        .args(["new", "--lib", &name])
+                        .status()
+                        .expect("`cargo new` command failed")
+                        .success(),
+                    "cannot create new project"
+                );
+            } else if !lib && xtask {
+                todo!("a new binary project using xtask");
+            } else if xtask && lib {
+                todo!("a new library project using xtask");
             }
         }
     }
