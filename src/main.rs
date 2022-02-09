bfff6f5b964fdddbeb60dd29da26475ca9efafa9
yozhgoor <yozhgoor@outlook.com>
2022-02-09 04:34:37 +0100

    Move subcommand behaviour into functions

--- src/main.rs
+++ src/main.rs
@@ -60,56 +60,7 @@ fn main() -> anyhow::Result<()> {
             path,
             command,
             no_terminal,
-        } => {
-            let working_dir = set_working_dir(path)?;
-
-            let mut main_process = if command.is_empty() {
-                let mut main_process = std::process::Command::new("nvim");
-                main_process.current_dir(&working_dir);
-                main_process.arg(".");
-
-                main_process
-            } else {
-                let mut it = command.iter();
-                let mut main_process = std::process::Command::new(it.next().unwrap());
-                main_process.current_dir(&working_dir);
-                main_process.args(it);
-
-                main_process
-            };
-
-            let terminal_process = if !no_terminal {
-                match std::process::Command::new("alacritty")
-                    .arg("--working-directory")
-                    .arg(working_dir.as_os_str())
-                    .spawn()
-                {
-                    Ok(child) => Some(child),
-                    Err(err) => {
-                        println!("an error occurred when launching alacritty: {}", err);
-                        None
-                    }
-                }
-            } else {
-                log::info!("Use the command directly instead");
-                None
-            };
-
-            anyhow::ensure!(
-                main_process
-                    .status()
-                    .expect("cannot launch main process")
-                    .success(),
-                "launch command failed"
-            );
-
-            if let Some(mut child) = terminal_process {
-                child.kill()?;
-                child.wait()?;
-            }
-
-            Ok(())
-        }
+        } => run_launch(path, command, no_terminal),
         Opt::Checks {
             path,
             check,
@@ -117,105 +68,7 @@ fn main() -> anyhow::Result<()> {
             test,
             clippy,
             clean,
-        } => {
-            let working_dir = set_working_dir(path)?;
-
-            if clean {
-                if std::process::Command::new("cargo")
-                    .current_dir(&working_dir)
-                    .arg("clean")
-                    .status()
-                    .expect("cannot launch `cargo clean`")
-                    .success()
-                {
-                    log::info!("Cleaned");
-                } else {
-                    log::error!("command `cargo clean` failed");
-                }
-            }
-
-            let mut check = if !check.is_empty() {
-                let mut command = std::process::Command::new("cargo");
-                command.current_dir(&working_dir).args(check);
-
-                command
-            } else {
-                let mut command = std::process::Command::new("cargo");
-                command
-                    .current_dir(&working_dir)
-                    .args(["check", "--workspace", "--all-features"]);
-
-                command
-            };
-
-            let mut test = if !test.is_empty() {
-                let mut command = std::process::Command::new("cargo");
-                command.current_dir(&working_dir).args(test);
-
-                command
-            } else {
-                let mut command = std::process::Command::new("cargo");
-                command
-                    .current_dir(&working_dir)
-                    .args(["test", "--workspace", "--all-features"]);
-
-                command
-            };
-
-            let mut fmt = if !fmt.is_empty() {
-                let mut command = std::process::Command::new("cargo");
-                command.current_dir(&working_dir).args(fmt);
-
-                command
-            } else {
-                let mut command = std::process::Command::new("cargo");
-                command
-                    .current_dir(&working_dir)
-                    .args(["fmt", "--all", "--check"]);
-
-                command
-            };
-
-            let mut clippy = if !clippy.is_empty() {
-                let mut command = std::process::Command::new("cargo");
-                command.current_dir(&working_dir).args(clippy);
-
-                command
-            } else {
-                let mut command = std::process::Command::new("cargo");
-                command
-                    .current_dir(&working_dir)
-                    .args(["clippy", "--all", "--tests", "--", "-D", "warnings"]);
-
-                command
-            };
-
-            if check.output()?.status.success() {
-                log::info!("cargo check : Ok");
-            } else {
-                log::error!("cargo check : Nope");
-            };
-
-            if test.output()?.status.success() {
-                log::info!("cargo test  : Ok");
-            } else {
-                log::error!("cargo test  : Nope");
-            };
-
-            if fmt.output()?.status.success() {
-                log::info!("cargo fmt   : Ok");
-            } else {
-                log::error!("cargo fmt   : Nope");
-            };
-
-            if clippy.output()?.status.success() {
-                log::info!("cargo clippy: Ok");
-            } else {
-                log::error!("cargo clippy: Nope");
-            };
-
-            Ok(())
-        }
+        } => run_checks(path, check, fmt, test, clippy, clean),
     }
 }
 
@@ -226,4 +79,166 @@ fn set_working_dir(path: Option<std::path::PathBuf>) -> anyhow::Result<std::path
     };
 
     Ok(working_dir)
+}
+
+fn run_launch(
+    path: Option<std::path::PathBuf>,
+    command: Vec<String>,
+    no_terminal: bool,
+) -> anyhow::Result<()> {
+    let working_dir = set_working_dir(path)?;
+
+    let mut main_process = if command.is_empty() {
+        let mut main_process = std::process::Command::new("nvim");
+        main_process.current_dir(&working_dir);
+        main_process.arg(".");
+
+        main_process
+    } else {
+        let mut it = command.iter();
+        let mut main_process = std::process::Command::new(it.next().unwrap());
+        main_process.current_dir(&working_dir);
+        main_process.args(it);
+
+        main_process
+    };
+
+    let terminal_process = if !no_terminal {
+        match std::process::Command::new("alacritty")
+            .arg("--working-directory")
+            .arg(working_dir.as_os_str())
+            .spawn()
+        {
+            Ok(child) => Some(child),
+            Err(err) => {
+                println!("an error occurred when launching alacritty: {}", err);
+                None
+            }
+        }
+    } else {
+        log::info!("Use the command directly instead");
+        None
+    };
+
+    anyhow::ensure!(
+        main_process
+            .status()
+            .expect("cannot launch main process")
+            .success(),
+        "launch command failed"
+    );
+
+    if let Some(mut child) = terminal_process {
+        child.kill()?;
+        child.wait()?;
+    }
+
+    Ok(())
+}
+
+fn run_checks(
+    path: Option<std::path::PathBuf>,
+    check: Vec<String>,
+    fmt: Vec<String>,
+    test: Vec<String>,
+    clippy: Vec<String>,
+    clean: bool,
+) -> anyhow::Result<()> {
+    let working_dir = set_working_dir(path)?;
+
+    if clean {
+        if std::process::Command::new("cargo")
+            .current_dir(&working_dir)
+            .arg("clean")
+            .status()
+            .expect("cannot launch `cargo clean`")
+            .success()
+        {
+            log::info!("Cleaned");
+        } else {
+            log::error!("command `cargo clean` failed");
+        }
+    }
+
+    let mut check = if !check.is_empty() {
+        let mut command = std::process::Command::new("cargo");
+        command.current_dir(&working_dir).args(check);
+
+        command
+    } else {
+        let mut command = std::process::Command::new("cargo");
+        command
+            .current_dir(&working_dir)
+            .args(["check", "--workspace", "--all-features"]);
+
+        command
+    };
+
+    let mut test = if !test.is_empty() {
+        let mut command = std::process::Command::new("cargo");
+        command.current_dir(&working_dir).args(test);
+
+        command
+    } else {
+        let mut command = std::process::Command::new("cargo");
+        command
+            .current_dir(&working_dir)
+            .args(["test", "--workspace", "--all-features"]);
+
+        command
+    };
+
+    let mut fmt = if !fmt.is_empty() {
+        let mut command = std::process::Command::new("cargo");
+        command.current_dir(&working_dir).args(fmt);
+
+        command
+    } else {
+        let mut command = std::process::Command::new("cargo");
+        command
+            .current_dir(&working_dir)
+            .args(["fmt", "--all", "--check"]);
+
+        command
+    };
+
+    let mut clippy = if !clippy.is_empty() {
+        let mut command = std::process::Command::new("cargo");
+        command.current_dir(&working_dir).args(clippy);
+
+        command
+    } else {
+        let mut command = std::process::Command::new("cargo");
+        command
+            .current_dir(&working_dir)
+            .args(["clippy", "--all", "--tests", "--", "-D", "warnings"]);
+
+        command
+    };
+
+    if check.output()?.status.success() {
+        log::info!("cargo check : Ok");
+    } else {
+        log::error!("cargo check : Nope");
+    };
+
+    if test.output()?.status.success() {
+        log::info!("cargo test  : Ok");
+    } else {
+        log::error!("cargo test  : Nope");
+    };
+
+    if fmt.output()?.status.success() {
+        log::info!("cargo fmt   : Ok");
+    } else {
+        log::error!("cargo fmt   : Nope");
+    };
+
+    if clippy.output()?.status.success() {
+        log::info!("cargo clippy: Ok");
+    } else {
+        log::error!("cargo clippy: Nope");
+    };
+
+    Ok(())
 }
