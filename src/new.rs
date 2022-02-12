293cc4fb6fbdf6a1c4618caa3becbccf664a8186
yozhgoor <yozhgoor@outlook.com>
2022-02-12 04:16:17 +0100

    Add the creation of licenses files from templates

--- src/new.rs
+++ src/new.rs
@@ -1,20 +1,63 @@
 /// Create a new Rust project with some defaults.
 #[derive(clap::Parser)]
 pub struct New {
-    /// Path where the project will be created (the name will be appended).
-    path: Option<std::path::PathBuf>,
     /// Name of the Rust project.
     name: String,
-    /// Create a new Rust library.
+    #[clap(short = 'p', long)]
+    /// Path where the project will be created (the name will be appended).
+    path: Option<std::path::PathBuf>,
+    /// Full name used in the licenses.
     #[clap(long)]
+    full_name: Option<String>,
+    /// Create a new Rust library.
+    #[clap(short = 'l', long)]
     lib: bool,
     /// Create a new Rust xtask project.
-    #[clap(long)]
+    #[clap(short = 'x', long)]
     xtask: bool,
+    /// Do not add licenses.
+    #[clap(long)]
+    no_license: bool,
+    /// Do not add CI.
+    #[clap(long)]
+    no_ci: bool,
 }
 
 impl New {
     pub fn run(self) -> anyhow::Result<()> {
-        todo!("New: run");
+        let working_dir = crate::set_working_dir(self.path)?;
+
+        let project_dir_path = working_dir.join(self.name);
+        std::fs::create_dir(&project_dir_path)?;
+
+        if !self.no_license {
+            let year = chrono::Utc::today();
+            let fullname = self
+                .full_name
+                .unwrap_or_else(|| "Yohan Boogaert".to_string());
+
+            std::fs::write(
+                &project_dir_path.join("LICENSE.mit"),
+                format!(
+                    include_str!("../templates/mit"),
+                    year = year,
+                    fullname = fullname
+                ),
+            )?;
+            std::fs::write(
+                &project_dir_path.join("LICENSE.Apache-2.0"),
+                format!(
+                    include_str!("../templates/apache"),
+                    year = year,
+                    fullname = fullname
+                ),
+            )?;
+        }
+
+        if !self.no_ci {
+            todo!("write CI file");
+        }
+
+        Ok(())
     }
 }
