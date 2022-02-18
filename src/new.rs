ae4e0aba2287d973b505ea02a5150fd0559e505f
yozhgoor <yozhgoor@outlook.com>
2022-02-18 01:16:32 +0100

    Generate CI with a new project

--- src/new.rs
+++ src/new.rs
@@ -37,6 +37,7 @@ impl New {
         std::fs::create_dir(&project_dir_path)?;
 
         if !self.no_license {
+            log::info!("Generating licenses");
             add_licenses(&project_dir_path, self.full_name)?;
         }
 
@@ -45,8 +46,10 @@ impl New {
             std::fs::create_dir_all(&workflows_dir)?;
 
             if self.lib {
+                log::info!("Generating lib's CI");
                 add_lib_ci(&workflows_dir, self.no_windows, self.no_osx)?;
             } else {
+                log::info!("Generating bin's CI");
                 add_bin_ci(&workflows_dir, &self.name, self.no_windows, self.no_osx)?;
             }
         }
@@ -67,7 +70,7 @@ fn add_licenses(
     std::fs::write(
         project_dir_path.join("LICENSE.MIT"),
         format!(
-            include_str!("../templates/mit"),
+            include_str!("../templates/licenses/mit"),
             year = year,
             full_name = full_name
         ),
@@ -75,7 +78,7 @@ fn add_licenses(
     std::fs::write(
         project_dir_path.join("LICENSE.Apache-2.0"),
         format!(
-            include_str!("../templates/apache"),
+            include_str!("../templates/licenses/apache"),
             year = year,
             full_name = full_name
         ),
@@ -89,8 +92,8 @@ fn add_lib_ci(
     no_windows: bool,
     no_osx: bool,
 ) -> anyhow::Result<()> {
-    let main_workflow = generate_main_workflow(no_windows, no_osx);
-    let pr_workflow = generate_pr_workflow(no_windows, no_osx);
+    let main_workflow = generate_main_workflow(no_windows, no_osx)?;
+    let pr_workflow = generate_pr_workflow(no_windows, no_osx)?;
 
     std::fs::write(workflows_dir.join("main.yml"), main_workflow)?;
     std::fs::write(workflows_dir.join("pr.yml"), pr_workflow)?;
@@ -104,9 +107,9 @@ fn add_bin_ci(
     no_windows: bool,
     no_osx: bool,
 ) -> anyhow::Result<()> {
-    let main_workflow = generate_main_workflow(no_windows, no_osx);
-    let pr_workflow = generate_pr_workflow(no_windows, no_osx);
-    let release_workflow = generate_release_workflow(project_name, no_windows, no_osx);
+    let main_workflow = generate_main_workflow(no_windows, no_osx)?;
+    let pr_workflow = generate_pr_workflow(no_windows, no_osx)?;
+    let release_workflow = generate_release_workflow(project_name, no_windows, no_osx)?;
 
     std::fs::write(workflows_dir.join("main.yml"), main_workflow)?;
     std::fs::write(workflows_dir.join("pr.yml"), pr_workflow)?;
@@ -115,14 +118,244 @@ fn add_bin_ci(
     Ok(())
 }
 
-fn generate_main_workflow(_no_windows: bool, _no_osx: bool) -> String {
-    todo!("generate main workflow");
+use std::fmt::Write;
+
+fn generate_main_workflow(no_windows: bool, no_osx: bool) -> anyhow::Result<String> {
+    let header = "name: main
+
+on:
+  push:
+    branches: [ main ]
+  schedule:
+    - cron: 0 0 1 * *
+
+env:
+  CARGO_TERM_COLOR: always
+
+jobs:
+";
+
+    let mut workflow = String::new();
+
+    write!(
+        workflow,
+        "{}{}",
+        header,
+        format!(
+            include_str!("../templates/ci/test"),
+            job_name = "test",
+            platform = "ubuntu-latest"
+        )
+    )?;
+
+    if !no_windows {
+        write!(
+            workflow,
+            "{}",
+            format!(
+                include_str!("../templates/ci/test"),
+                job_name = "test-windows",
+                platform = "windows-latest"
+            )
+        )?;
+    }
+
+    if !no_osx {
+        write!(
+            workflow,
+            "{}",
+            format!(
+                include_str!("../templates/ci/test"),
+                job_name = "test-osx",
+                platform = "windows-latest"
+            )
+        )?;
+    }
+
+    Ok(workflow)
 }
 
-fn generate_pr_workflow(_no_windows: bool, _no_osx: bool) -> String {
-    todo!("generate pr workflow");
+fn generate_pr_workflow(no_windows: bool, no_osx: bool) -> anyhow::Result<String> {
+    let header = "name: PR
+
+on:
+  pull_request:
+    branches: [ main ]
+
+env:
+  CARGO_TERM_COLOR: always
+
+jobs:
+";
+
+    let mut workflow = String::new();
+
+    write!(
+        workflow,
+        "{}",
+        header,
+    )?;
+    write!(
+        workflow,
+        include_str!("../templates/ci/test_and_lint"),
+        job_name = "test-and-lint",
+        platform = "ubuntu-latest"
+    )?;
+
+    if !no_windows {
+        write!(
+            workflow,
+                include_str!("../templates/ci/test"),
+                job_name = "test-windows",
+                platform = "windows-latest"
+        )?;
+    }
+
+    if !no_osx {
+        write!(
+            workflow,
+            include_str!("../templates/ci/test"),
+            job_name = "test-osx",
+            platform = "windows-latest"
+        )?;
+    }
+
+    Ok(workflow)
 }
 
-fn generate_release_workflow(_project_name: &str, _no_windows: bool, _no_osx: bool) -> String {
-    todo!("generate release workflow");
+fn generate_release_workflow(project_name: &str, no_windows: bool, no_osx: bool) -> anyhow::Result<String> {
+   let build_linux_name = "build-linux";
+   let build_windows_name = "build-windows";
+   let build_osx_name = "build-osx";
+
+   let header = "name: Release
+
+on:
+  push:
+    tags:
+      - 'v*'
+
+jobs:
+";
+
+    let mut workflow = String::new();
+
+    write!(
+        workflow,
+        "{}",
+        header,
+    )?;
+    write!(
+        workflow,
+        include_str!("../templates/ci/release_build_with_strip"),
+        job_name = build_linux_name,
+        platform = "ubuntu-latest",
+        target = "x86_64-unknown-linux-gnu",
+        build_title = "Build release (Windows)",
+        build_name = build_linux_name,
+        project_name = project_name,
+    )?;
+
+    if !no_windows {
+        write!(
+            workflow,
+            include_str!("../templates/ci/release_build"),
+            job_name = build_windows_name,
+            platform = "windows-latest",
+            target = "x86_64-windows-msvc",
+            build_title = "Build release (Windows)",
+            build_name = build_windows_name,
+            project_name = project_name,
+        )?;
+    }
+
+    if !no_osx {
+        write!(
+            workflow,
+            include_str!("../templates/ci/release_build"),
+            job_name = build_osx_name,
+            platform = "macos-latest",
+            target = "x86_64-apple-darwin",
+            build_title = "Build release (OSX)",
+            build_name = build_osx_name,
+            project_name = project_name,
+        )?;
+    }
+
+    let needs = if no_windows && no_osx {
+        format!("[{}]", build_linux_name)
+    } else if !no_windows && no_osx {
+        format!("[{}, {}]", build_linux_name, build_windows_name)
+    } else if no_windows && !no_osx {
+        format!("[{}, {}]", build_linux_name, build_osx_name)
+    } else {
+        format!("[{}, {}, {}]", build_linux_name, build_windows_name, build_osx_name)
+    };
+
+    write!(
+        workflow,
+        include_str!("../templates/ci/release_start"),
+        needs = needs,
+    )?;
+
+    write!(
+        workflow,
+        include_str!("../templates/ci/release_step"),
+        build_name = build_linux_name,
+        build_path = build_linux_name,
+        project_name = project_name,
+        platform = "linux-x86_64"
+    )?;
+
+    if !no_windows {
+        write!(
+            workflow,
+            include_str!("../templates/ci/release_step"),
+            build_name = build_windows_name,
+            build_path = build_windows_name,
+            project_name = project_name,
+            platform = "windows-x86_64"
+        )?;
+    }
+
+    if !no_osx {
+        write!(
+            workflow,
+            include_str!("../templates/ci/release_step"),
+            build_name = build_osx_name,
+            build_path = build_osx_name,
+            project_name = project_name,
+            platform = "osx-x86_64"
+        )?;
+    }
+
+    let files = if no_windows && no_osx {
+        format!("|
+            {}/*
+", build_linux_name)
+    } else if !no_windows && no_osx {
+        format!("|
+            {}/*
+            {}/*
+", build_linux_name, build_windows_name)
+    } else if no_windows && !no_osx {
+        format!("|
+            {}/*
+            {}/*
+", build_linux_name, build_osx_name)
+    } else {
+        format!("|
+            {}/*
+            {}/*
+            {}/*
+", build_linux_name, build_windows_name, build_osx_name)
+    };
+
+    write!(
+        workflow,
+        include_str!("../templates/ci/release_end"),
+        files = files
+    )?;
+
+    Ok(workflow)
 }
