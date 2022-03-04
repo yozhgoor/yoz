d90b64b46e2800e475d03ed595a1006da6e3f31e
yozhgoor <yozhgoor@outlook.com>
2022-03-04 23:02:46 +0100

    Improve the implementation of the `install` subcommand.

--- src/install.rs
+++ src/install.rs
@@ -20,29 +20,96 @@ impl Install {
     pub fn run(self) -> Result<()> {
         if let Some(name) = self.name {
             if self.cargo {
-                process::Command::new("cargo")
-                    .args(["install", &name])
-                    .status()?;
-            } else if let Some(url) = self.aur {
-                let builds_dir = dirs::home_dir().expect("cannot get home directory");
-                process::Command::new("git")
-                    .current_dir(&builds_dir)
-                    .args(["clone", &url])
-                    .status()?;
-                process::Command::new("makepkg")
-                    .current_dir(&builds_dir.join(&name))
-                    .args(["--syncdeps", "--install", "--clean"]);
+                Manager::Cargo.check_or_install(name, None)?;
+            } else if self.aur.is_some() {
+                Manager::Aur.check_or_install(name, self.aur)?;
             } else {
-                process::Command::new("sudo")
-                    .args(["pacman", "--sync", &name])
-                    .status()?;
+                Manager::Pacman.check_or_install(name, None)?;
             }
         } else {
             if self.prelude {
-                unimplemented!();
+                let cargo_programs = vec![
+                    "alacritty",
+                    "bat",
+                    "cargo-rdme",
+                    "cargo-release",
+                    "cargo-temp",
+                    "cargo-update",
+                    "cargo-watch",
+                    "mdbook",
+                    "startship",
+                ];
+
+                let mut aur_programs = std::collections::HashMap::new();
+                aur_programs.insert("spotify", "https://aur.archlinux.org/spotify.git");
+
+                let pacman_programs = vec![
+                    "discord",
+                    "feh",
+                    "flameshot",
+                    "neovim",
+                ];
+
+                for program in cargo_programs {
+                    Manager::Cargo.check_or_install(program.to_string(), None)?;
+                }
+
+                for (program, url) in aur_programs {
+                    Manager::Aur.check_or_install(program.to_string(), Some(url.to_string()))?;
+                }
+
+                for program in pacman_programs {
+                    Manager::Pacman.check_or_install(program.to_string(), None)?;
+                }
             } else {
                 log::error!("Please select something to install");
             }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+enum Manager {
+    Aur,
+    Cargo,
+    Pacman,
+}
+
+impl Manager {
+    fn check_or_install(self, program: String, url: Option<String>) -> Result<()> {
+        let check_status = process::Command::new(&program).arg("--version").status();
+
+        if let Err(_) = check_status {
+            log::info!("Installing {}", &program);
+            match self {
+                Self::Aur => {
+                    let builds_dir = dirs::home_dir().expect("cannot get home directory");
+                    let url = url.expect("Cannot install via AUR without url");
+                    process::Command::new("git")
+                        .current_dir(&builds_dir)
+                        .args(["clone", &url])
+                        .status()?;
+                    process::Command::new("makepkg")
+                        .current_dir(&builds_dir.join(&program))
+                        .args(["--syncdeps", "--install", "--clean"]);
+                }
+                Self::Cargo => {
+                    process::Command::new("cargo")
+                        .args(["install", &program])
+                        .status()?;
+                }
+                Self::Pacman => {
+                    process::Command::new("sudo")
+                        .args(["pacman", "--sync", &program])
+                        .status()?;
+                }
+            }
+        } else if check_status?.success() {
+            log::info!("{} is already installed", &program);
+        } else {
+            log::error!("an error occurred when checking {}", &program);
         }
 
         Ok(())
