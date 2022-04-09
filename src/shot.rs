90599393b6e1d64ed54aedb42cf3e3f932c9aed7
yozhgoor <yozhgoor@outlook.com>
2022-04-10 01:34:27 +0200

    Implement the behaviour of the `shot` subcommand

--- src/shot.rs
+++ src/shot.rs
@@ -1,33 +1,64 @@
-use std::path::PathBuf;
-use anyhow::Result;
+use anyhow::{ensure, Result};
+use std::{path::PathBuf, process};
 
 /// Take a screenshot using flameshot.
 ///
 /// If nothing is provided as argument, this will open the
 /// GUI mode.
-#[derive(clap::Parser)]
+#[derive(Debug, clap::Parser)]
 pub struct Shot {
     /// Show a file for copy using bat.
     #[clap(long)]
     text: Option<PathBuf>,
     /// Capture a single screen.
     #[clap(long)]
-    screen: bool,
+    screen: Option<bool>,
     /// Capture the entire desktop.
     #[clap(long)]
-    desktop: bool,
+    desktop: Option<bool>,
 }
 
 impl Shot {
     pub fn run(self) -> Result<()> {
         if let Some(path) = self.text {
-            todo!("bat path --style plain");
-        } else if self.screen {
-            todo!("flameshot screen");
-        } else if self.desktop {
-            todo!("flameshot full");
+            ensure!(
+                process::Command::new("bat")
+                    .arg(&path)
+                    .arg("--style")
+                    .arg("plain")
+                    .status()?
+                    .success(),
+                "cannot open {} with bat",
+                path.display()
+            );
+        } else if let Some(clipboard) = self.screen {
+            let mut process = process::Command::new("flameshot");
+            process.arg("screen");
+
+            if clipboard {
+                process.arg("--clipboard");
+            }
+
+            ensure!(process.status()?.success(), "`flameshot screen` failed");
+        } else if let Some(clipboard) = self.desktop {
+            let mut process = process::Command::new("flameshot");
+            process.arg("full");
+
+            if clipboard {
+                process.arg("--clipboard");
+            }
+
+            ensure!(process.status()?.success(), "`flameshot full` failed");
         } else {
-            todo!("flameshot gui");
+            ensure!(
+                process::Command::new("flameshot")
+                    .arg("gui")
+                    .status()?
+                    .success(),
+                "`flameshot gui` failed"
+            );
         }
+
+        Ok(())
     }
 }
